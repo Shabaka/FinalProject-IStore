a654ee8ff050520a205affb245deb2d8080b4c0f
Ахмет Шабакаев <45225080+Shabaka@users.noreply.github.com>
2019-03-24 20:36:10 +0300

    240319
    update project 240319

---
+++ js/catalog.js
@@ -0,0 +1,6 @@
+var productsCatalog = [
+    { id: 'el1', name: 'Mi MIX 3 6/128GB', price: 44900, img: 'img/1.jpg' },
+    { id: 'el2', name: 'Mi A2 Lite 4/64', price: 15990, img: 'img/4.jpg' },
+    { id: 'el3', name: 'Xiaomi Pocophone F1', price: 24990, img: 'img/8.jpg' },
+    { id: 'el4', name: 'Redmi 6 3/64GB', price: 10990, img: 'img/10.jpg' }
+];
---
+++ js/serviceProducts.js
@@ -0,0 +1,75 @@
+//слайдер с перемоткой
+var imgStore = ['img/slider/1.jpg', 'img/slider/2.jpg', 'img/slider/3.jpg', 'img/slider/4.jpg', 'img/slider/5.jpg', 'img/slider/6.jpg'];
+
+var img = document.querySelector('.img');
+img.setAttribute('src', imgStore[0]);
+img.setAttribute('data-index', 0);
+
+document.querySelector('.btn_right').addEventListener('click', function () {//Кнопка "Вперед"
+    var index = Number(img.getAttribute('data-index'));
+
+    if (index == imgStore.length - 1) {
+        index = -1;
+    }
+    img.setAttribute('src', imgStore[++index]);
+    img.setAttribute('data-index', index++);
+});
+
+document.querySelector('.btn_left').addEventListener('click', function () {//Кнопка "Назад"
+    index = Number(img.getAttribute('data-index'));
+
+    if (index == 0) {
+        index = imgStore.length;
+    }
+    img.setAttribute('src', imgStore[--index]);
+    img.setAttribute('data-index', index--);
+});
+//слайдер с перемоткой
+
+//витрина
+class ServiceProducts {
+    constructor(containerProducts, productsCatalog) {
+        this.container = document.querySelector(containerProducts);
+        this.productsCatalog = productsCatalog;
+        this.create();
+    }
+    create() {
+        var wrapper = document.createElement('slot');
+
+        for (var i = 0; i < this.productsCatalog.length; i++) {
+
+            var item = this.getElement({ tagName: 'div', className: 'item' });
+            var name = this.getElement({ tagName: 'div', className: 'name', innerText: this.productsCatalog[i].name });
+            var img = this.getElement({ tagName: 'div', className: 'img', backgroundImage: `url(${this.productsCatalog[i].img})` });
+            var price = this.getElement({ tagName: 'div', className: 'price', innerText: this.productsCatalog[i].price.toLocaleString() + ' ₽' });
+            var btn = this.getElement({ tagName: 'button', className: 'btn', innerText: 'Купить' });
+
+            item.appendChild(name);
+            item.appendChild(img);
+            item.appendChild(price);
+            item.appendChild(btn);
+            wrapper.appendChild(item);
+        }
+        this.container.appendChild(wrapper);
+    }
+
+    getElement(options) {
+        var element = document.createElement(options.tagName);
+        if ('className' in options) {
+            element.setAttribute('class', options.className);
+        }
+        if ('innerText' in options) {
+            element.innerText = options.innerText;
+        }
+        if ('backgroundImage' in options) {
+            element.style.backgroundImage = options.backgroundImage;
+        }
+        return element;
+    }
+
+    actions() {
+    }
+}
+
+var serviceProducts = new ServiceProducts('.container-products', productsCatalog);
+//витрина
